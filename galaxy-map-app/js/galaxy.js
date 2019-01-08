be7eea231f034f15ded9bb3f3fdb13c990d50fcc
Naftali <weiss@Naftalis-iMac.fios-router.home>
2019-01-08 13:18:53 -0500

    made a div look like a planet

--- galaxy-map-app/js/galaxy.js
+++ galaxy-map-app/js/galaxy.js
@@ -11,22 +11,22 @@ $(() => {
 
 
 
-    $('.planet').on('click',(clkd) => {
-      let i = $(clkd.currentTarget).attr('id')
-
-      $.ajax(
-         {
-           url: 'https://swapi.co/api/planets'
-         }
-       ).then((data) => {
-           // for (var i = 0; i < data.results.length; i++) {
-             console.log(data.results[i].name)
-           // }
-         },
-         () => {
-           console.log("Error");
-         })
-    })
+    // $('.planet').on('click',(clkd) => {
+    //   // let i = $(clkd.currentTarget).attr('id')
+    //
+    //   $.ajax(
+    //      {
+    //        url: 'https://swapi.co/api/planets'
+    //      }
+    //    ).then((data) => {
+    //        for (var i = 0; i < data.results.length; i++) {
+    //          console.log(data.results[i].name)
+    //        }
+    //      },
+    //      () => {
+    //        console.log("Error");
+    //      })
+    // })
 
 
 
