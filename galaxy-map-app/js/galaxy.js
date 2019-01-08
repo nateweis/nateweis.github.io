8d7f09cb251563a8a9712512876e64c6a3050b72
Naftali <weiss@Naftalis-iMac.fios-router.home>
2019-01-08 15:04:54 -0500

    made some zoom functionality on the buttons

--- galaxy-map-app/js/galaxy.js
+++ galaxy-map-app/js/galaxy.js
@@ -1,32 +1,57 @@
 $(() => {
 
 
+  // //////////////////////////////////////////
+  // Zoom Buttons Functionality
+  // /////////////////////////////////////////
+  const $in = $('#zoom-in')
+  const $out = $('#zoom-out')
+  const $space = $('.space')
+  let zoom = 1;
+
+  const zoomIn = () => {
+    zoom += .4;
+    $space.css('zoom',zoom);
+    if($space.css('zoom') >= 2){
+      $space.css('background','darkgreen')
+    }
+  }
+  const zoomOut = () => {
+    if(zoom > .4){
+      zoom -= .4;
+      $space.css('zoom',zoom);
+      if($space.css('zoom') < 2){
+        $space.css('background','steelblue')
+      }
+    }
+  }
+
+  $in.on('click',zoomIn)
+  $out.on('click',zoomOut)
 
 
 
 
+    // //////////////////////////////////////////
+    // Planet Click Functionality
+    // /////////////////////////////////////////
 
+    $('.planet').on('click',(clkd) => {
+      let i = $(clkd.currentTarget).attr('id')
 
-
-
-
-
-    // $('.planet').on('click',(clkd) => {
-    //   // let i = $(clkd.currentTarget).attr('id')
-    //
-    //   $.ajax(
-    //      {
-    //        url: 'https://swapi.co/api/planets'
-    //      }
-    //    ).then((data) => {
-    //        for (var i = 0; i < data.results.length; i++) {
-    //          console.log(data.results[i].name)
-    //        }
-    //      },
-    //      () => {
-    //        console.log("Error");
-    //      })
-    // })
+      $.ajax(
+         {
+           url: 'https://swapi.co/api/planets'
+         }
+       ).then((data) => {
+          // for (var i = 0; i < data.results.length; i++) {
+             console.log(data.results[i].name)
+          // }
+         },
+         () => {
+           console.log("Error");
+         })
+    })
 
 
 
