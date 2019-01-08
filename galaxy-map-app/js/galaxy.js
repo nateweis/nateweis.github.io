4cbc7587fd5e36ce289a5751cdfca583358e12b8
Naftali <weiss@Naftalis-iMac.fios-router.home>
2019-01-08 11:50:56 -0500

    inital commit

---
+++ galaxy-map-app/js/galaxy.js
@@ -0,0 +1,28 @@
+$(() => {
+
+
+
+
+
+
+
+
+
+ $.ajax(
+    {
+      url: 'https://swapi.co/api/planets'
+    }
+  ).then((data) => {
+    for (var i = 0; i < data.results.length; i++) {
+      console.log(data.results[i].name)
+    }
+  },
+  () => {
+    console.log("Error");
+  })
+
+
+
+
+
+})
