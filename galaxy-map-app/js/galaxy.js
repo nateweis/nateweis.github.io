114f35b9458f7848782a4f6b34934874216b7f50
Naftali <weiss@Naftalis-iMac.fios-router.home>
2019-01-08 12:25:20 -0500

    planets display when div is clicked

--- galaxy-map-app/js/galaxy.js
+++ galaxy-map-app/js/galaxy.js
@@ -8,20 +8,25 @@ $(() => {
 
 
 
- $.ajax(
-    {
-      url: 'https://swapi.co/api/planets'
-    }
-  ).then((data) => {
-    for (var i = 0; i < data.results.length; i++) {
-      console.log(data.results[i].name)
-    }
-  },
-  () => {
-    console.log("Error");
-  })
 
 
+
+    $('div').on('click',(clkd) => {
+      let i = $(clkd.currentTarget).attr('id')
+
+      $.ajax(
+         {
+           url: 'https://swapi.co/api/planets'
+         }
+       ).then((data) => {
+           // for (var i = 0; i < data.results.length; i++) {
+             console.log(data.results[i].name)
+           // }
+         },
+         () => {
+           console.log("Error");
+         })
+    })
 
 
 
